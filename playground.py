f94b68192d7df225e6c1cdb5865860514f946751
Allan Pais <62473329+Allan0820@users.noreply.github.com>
2024-01-30 18:05:47 +0530

    Deleted file playground.py which was used for testing a small block of code.

--- playground.py
+++
@@ -1,9 +0,0 @@
-def init_config():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, required=True)
-    args = parser.parse_args()
-    config = process_config(args.config)
-
-    return config
-
-print(config.load_checkpoint)
